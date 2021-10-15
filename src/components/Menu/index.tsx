d23a0317ed01ace4ebbf6f8a49d4423c93131be3
Demari C <demaricameron@outlook.com>
2021-10-14 20:54:43 -0400

    Add Limit Orders

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -62,11 +62,9 @@ const Menu: React.FC = (props) => {
             <NavLink className="nav_link" activeClassName="active" to="/swap">
               <li>Swap</li>
             </NavLink>
-            {isBSC && (
-              <NavLink className="nav_link" activeClassName="active" to="/limit-order">
-                <li>Limit Order</li>
-              </NavLink>
-            )}
+            <NavLink className="nav_link" activeClassName="active" to="/limit-order">
+              <li>Limit Order</li>
+            </NavLink>
             {/* {isBSC && (
               <NavLink className="nav_link" activeClassName="active" to="/stop-loss">
                 <li>Stop Loss</li>
