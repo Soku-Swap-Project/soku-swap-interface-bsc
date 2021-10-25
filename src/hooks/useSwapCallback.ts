5db3520193f16880d9adfbb015a3d7a2b1d4f962
Demari C <demaricameron@outlook.com>
2021-10-25 15:23:16 -0400

    test

--- src/hooks/useSwapCallback.ts
+++ src/hooks/useSwapCallback.ts
@@ -449,7 +449,7 @@ export function useSwapCallback(
           // @ts-ignore
           if (
             TRASNFER_FEE_TOKEN_ADDRESS_LIST[chainId || ChainId.MAINNET].includes(
-              trade?.inputAmount?.currency?.address || 'NonAddress'
+              trade.inputAmount.currency.address || 'NonAddress'
             )
           ) {
             throw new Error("Fee On Transfer isn't supported for limits and stops")
