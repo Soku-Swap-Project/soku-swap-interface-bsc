58ff58a8cb2a79f996cc8e32be29de262a7d3ae7
Demari C <demaricameron@outlook.com>
2021-12-13 14:51:38 +0200

    minor ui updates

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -6,7 +6,7 @@ const StyledNotFound = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
-  height: calc(100vh - 64px);
+  height: calc(90vh - 64px);
   justify-content: center;
 `
 
