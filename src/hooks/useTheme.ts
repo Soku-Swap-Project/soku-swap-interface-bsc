191d2a68a8c43ac1dc07e9cb63e9066c62fdb56c
Demari C <demaricameron@outlook.com>
2022-04-07 17:46:15 -0400

    Update useTheme.ts

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -3,9 +3,10 @@ import { ThemeContext as StyledThemeCopntext } from 'styled-components'
 import { ThemeContext } from '../ThemeContext'
 
 const useTheme = () => {
-  const { isDark, toggleTheme } = useContext(ThemeContext)
+  // const { isDark, toggleTheme } = useContext(ThemeContext)
   const theme = useContext(StyledThemeCopntext)
-  return { isDark, toggleTheme, theme }
+  // return { isDark, toggleTheme, theme }
+  return { theme }
 }
 
 export default useTheme
